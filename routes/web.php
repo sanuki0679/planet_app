3240c338cc729cd93270583dc3f14b04e3170745
sanuki0679 <sanuki0679@gmail.com>
2023-04-22 22:46:50 +0900

    Renderデプロイのための設定2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\MemoController::class, 'index']);
+Route::get('/', [App\Http\Controllers\PlanetController::class, 'index']);
 
 Route::resource('planets', App\Http\Controllers\PlanetController::class);
